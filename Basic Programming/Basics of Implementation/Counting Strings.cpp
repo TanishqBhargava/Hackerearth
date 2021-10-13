5d78b459f40d5440dc2e0a48d408f920af766fd2
Tanishq Bhargava <66817044+TanishqBhargava@users.noreply.github.com>
2021-10-13 17:15:17 +0530

    Counting Strings.cpp

---
+++ Basic Programming/Basics of Implementation/Counting Strings.cpp
@@ -0,0 +1,30 @@
+/*========================*\
+|  ID: tanishqbhargava2509 |
+|  LANG: C++               |
+\*========================*/
+//Tanishq Bhargava
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL);
+	long long int t; 
+    cin >> t; 
+    while(t--)
+	{
+		string s;
+		cin >> s;
+		long long int A[s.size()], index = s.size();
+		for(long long int i = s.size() - 1; i >= 0; i--)
+		{
+			if(s[i] == 'a' || s[i] == 'z')
+			    index = i;
+			A[i] = index;
+		}
+		long long int ans = 0;
+		for(long long int i = 0; i < s.size(); i++)
+			ans += s.size() - A[i];
+		cout << ans << endl;
+	}
+}

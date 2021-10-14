ebe66aa344cf58fd4f749a1bb2c5bccfb6947b44
Tanishq Bhargava <66817044+TanishqBhargava@users.noreply.github.com>
2021-10-14 16:02:33 +0530

    Guess the permutation.cpp

---
+++ Basic Programming/Basics of Implementation/Guess the permutation.cpp
@@ -0,0 +1,22 @@
+/*========================*\
+|  ID: tanishqbhargava2509 |
+|  LANG: C++               |
+\*========================*/
+//Tanishq Bhargava
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    string s;
+    int i, j;
+    cin >> s;
+    if (s == "ELEMENT")
+    {
+        cin >> i >> j;
+        cout << j;
+    }
+    else if (s == "PERMUTATION")
+        cout << "13 15 1 6 14 4 5 7 2 16 9 11 10 12 8 3";
+    return 0;
+}

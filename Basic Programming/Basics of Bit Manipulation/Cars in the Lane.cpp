43444757fe5e247cbc91019a5a6a62ba2f69b493
Tanishq Bhargava <66817044+TanishqBhargava@users.noreply.github.com>
2021-10-17 12:04:48 +0530

    Cars in the Lane.cpp

---
+++ Basic Programming/Basics of Bit Manipulation/Cars in the Lane.cpp
@@ -0,0 +1,25 @@
+/*========================*\
+|  ID: tanishqbhargava2509 |
+|  LANG: C++               |
+\*========================*/
+//Tanishq Bhargava
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    long long t;
+    cin >> t;
+    while(t--)
+    {
+        long long n, ans = 0;
+        cin >> n;
+        while(n)
+        {
+            n &= (n - 1);
+            ans += 1;
+        }
+        cout << (1<<ans) << endl;
+    }
+}

8ef5517cffc901d42207da9cbfa9c86a2ef5252e
Tanishq Bhargava <66817044+TanishqBhargava@users.noreply.github.com>
2021-10-19 11:01:32 +0530

    A subset in a sequence.py

---
+++ Basic Programming/Basics of Bit Manipulation/A subset in a sequence.py
@@ -0,0 +1,14 @@
+#=========================#
+# ID: tanishqbhargava2509 #
+# LANG: C++               #
+#=========================#
+#Tanishq Bhargava
+t=int(input())
+for _ in range(t):
+    n=(bin(int(input()))[2:])[::-1]
+    arr=[]
+    for i in range(len(n)):
+        if n[i]=='1':
+            arr.append(3**i)
+    print(len(arr))
+    print(*arr)

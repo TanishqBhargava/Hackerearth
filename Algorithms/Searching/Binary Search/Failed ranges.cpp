3613ac8886334fbb5a9b403020f485377ecf4f22
Tanishq Bhargava <66817044+TanishqBhargava@users.noreply.github.com>
2021-10-23 19:16:08 +0530

    Failed ranges.cpp

---
+++ Algorithms/Searching/Binary Search/Failed ranges.cpp
@@ -0,0 +1,21 @@
+/*========================*\
+|  ID: tanishqbhargava2509 |
+|  LANG: C++               |
+\*========================*/
+//Tanishq Bhargava
+// Compile & test is failed but when submitted gives correct result something wrong with problem
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() 
+{
+    ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    int n, m, X, Y;
+    cin >> n >> m;
+    vector<int> a(n), b(m);
+    for(int i = 0; i < n; i++) 
+        cin >> a[i];
+    for(int i = 0; i < m; i++) 
+        cin >> b[i];
+    cin >> X >> Y;
+}

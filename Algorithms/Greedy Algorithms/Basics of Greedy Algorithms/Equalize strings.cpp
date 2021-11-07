ea77145eb12447cb5cbc3c69687bcf994fb90dc7
Tanishq Bhargava <66817044+TanishqBhargava@users.noreply.github.com>
2021-11-07 21:23:24 +0530

    Update Equalize strings.cpp

--- Algorithms/Greedy Algorithms/Basics of Greedy Algorithms/Equalize strings.cpp
+++ Algorithms/Greedy Algorithms/Basics of Greedy Algorithms/Equalize strings.cpp
@@ -8,20 +8,20 @@ using namespace std;
  
 int main()
 {
-  	ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
-  	int n, cost = 0;
-  	cin >> n;
-  	string a, b;
-  	cin >> a >> b;
-  	a[n] = b[n] = '0';
-  	for(int i = 0; i < n; i++)
-  	{
-    	  if(a[i] != b[i])
-    	  {
-      		  cost++;
-      		  if(a[i] != a[i + 1] && b[i + 1] != a[i + 1])
-			  	      i++;
-    	  }
-  	}
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int n, cost = 0;
+    cin >> n;
+    string a, b;
+    cin >> a >> b;
+    a[n] = b[n] = '0';
+    for(int i = 0; i < n; i++)
+    {
+        if(a[i] != b[i])
+    	{
+      	    cost++;
+      	    if(a[i] != a[i + 1] && b[i + 1] != a[i + 1])
+	        i++;
+    	}
+    }
     cout << cost << endl;
 }

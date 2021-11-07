c2f7e089e45e24dcdc80e45b668793fc90c28604
Tanishq Bhargava <66817044+TanishqBhargava@users.noreply.github.com>
2021-11-07 18:34:45 +0530

    Filling stones.cpp

---
+++ Algorithms/Greedy Algorithms/Basics of Greedy Algorithms/Filling stones.cpp
@@ -0,0 +1,20 @@
+/*========================*\
+|  ID: tanishqbhargava2509 |
+|  LANG: C++               |
+\*========================*/
+//Tanishq Bhargava
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{ 
+	  ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int n;
+    cin >> n;
+    if(n == 1)
+		    cout << 2 << endl;
+    else if(n == 2)
+		    cout << 1 << endl;
+    else
+		    cout << 0 << endl;
+}

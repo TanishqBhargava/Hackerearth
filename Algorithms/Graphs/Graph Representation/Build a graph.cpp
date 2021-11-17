84d992d531c0ce6387860f08847c0a7ec8a0dd17
Tanishq Bhargava <66817044+TanishqBhargava@users.noreply.github.com>
2021-11-17 12:55:10 +0530

    Build a graph.cpp

---
+++ Algorithms/Graphs/Graph Representation/Build a graph.cpp
@@ -0,0 +1,17 @@
+/*========================*\
+|  ID: tanishqbhargava2509 |
+|  LANG: C++               |
+\*========================*/
+//Tanishq Bhargava
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	if(n < 12)
+		cout << "No";
+	else
+		cout << "Yes";
+}

e7f6f727cb60af7c2b3639aa84c2d30ad315c340
Tanishq Bhargava <66817044+TanishqBhargava@users.noreply.github.com>
2021-12-05 11:25:47 +0530

    Rotation.cpp

---
+++ Algorithms/String Algorithms/Basics of String Manipulation/Rotation.cpp
@@ -0,0 +1,26 @@
+/*========================*\
+|  ID: tanishqbhargava2509 |
+|  LANG: C++               |
+\*========================*/
+//Tanishq Bhargava
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	int n, count = 0;
+	cin >> n;
+	string s, t, tmp;
+	cin >> s >> t;
+	for(int i = 0; i < n; i++)
+	{
+		tmp = s.substr(i);
+		if(t.find(tmp) == 0)
+		{
+			cout << count << endl;
+			return 0;
+		}
+		count++;
+	}
+}

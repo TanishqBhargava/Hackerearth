ac4d2f9310f063630a6940c651bdd1681bc22651
Tanishq Bhargava <66817044+TanishqBhargava@users.noreply.github.com>
2021-12-05 19:57:48 +0530

    Caesar's Cipher.cpp

---
+++ Algorithms/String Algorithms/Basics of String Manipulation/Caesar's Cipher.cpp
@@ -0,0 +1,37 @@
+/*========================*\
+|  ID: tanishqbhargava2509 |
+|  LANG: C++               |
+\*========================*/
+//Tanishq Bhargava
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	int t;
+    cin >> t;
+    while(t--)
+    { 
+		string s, t;
+      	cin >> s >> t;
+     	int f = 1, ans = -1;
+     	for(int i = 0; i < s.size(); i++)
+     	{   
+		 	int diff = ((t[i] - s[i]) + 26) % 26;
+            if(ans == -1)
+            	ans = diff;
+          	else if(ans == diff)
+           		continue;
+           	else
+      		{     	
+				f = 0;
+            	break;
+        	}  
+        }
+		if(f)
+      		cout << ans << endl;
+      	else
+      		cout << -1 << endl;
+    }
+}

ef4a751491b3ae21748e59a4dc825c247c310eae
Tanishq Bhargava <66817044+TanishqBhargava@users.noreply.github.com>
2021-12-05 21:53:07 +0530

    Lexical Analyzer.cpp

---
+++ Algorithms/String Algorithms/Basics of String Manipulation/Lexical Analyzer.cpp
@@ -0,0 +1,30 @@
+/*========================*\
+|  ID: tanishqbhargava2509 |
+|  LANG: C++               |
+\*========================*/
+//Tanishq Bhargava
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() 
+{
+    ios_base::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+    long long n;
+    unordered_map<string, bool> mp;
+    cin >> n;
+    while(n-->=0)
+	{
+        string s;
+        getline(cin, s);
+        long long i = 0;
+        while(i < s.size())
+		{
+            if(s[i] == '=') 
+				break;
+            i++;
+        }
+        mp[s.substr(0, i)] = true;
+    }
+    cout << mp.size() - 1 << endl;
+    return 0;
+}

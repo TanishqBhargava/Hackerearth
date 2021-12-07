acd03657ad74c6d8f73479ad132753148f911043
Tanishq Bhargava <66817044+TanishqBhargava@users.noreply.github.com>
2021-12-07 18:34:58 +0530

    Ashish and Binary Matrix.cpp

---
+++ Algorithms/String Algorithms/Basics of String Manipulation/Ashish and Binary Matrix.cpp
@@ -0,0 +1,30 @@
+/*========================*\
+|  ID: tanishqbhargava2509 |
+|  LANG: C++               |
+\*========================*/
+//Tanishq Bhargava
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	int t, n, m;
+	cin >> t;
+	while(t--)
+	{
+		cin >> n >> m;
+    	string s;
+    	set<string> st;
+		for(int i = 0; i < n; i++)
+		{
+			cin >> s;
+			st.insert(s);
+        }
+        if(st.size() == n)
+			cout << "Yes" << endl;
+        else
+			cout << "No" << endl;
+	}
+	return 0;
+}

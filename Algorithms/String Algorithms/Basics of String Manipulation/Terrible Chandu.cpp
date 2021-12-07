65cfd00b875ce52f64d39a9bbf7b4126d891ab8e
Tanishq Bhargava <66817044+TanishqBhargava@users.noreply.github.com>
2021-12-07 10:48:51 +0530

    Terrible Chandu.cpp

---
+++ Algorithms/String Algorithms/Basics of String Manipulation/Terrible Chandu.cpp
@@ -0,0 +1,21 @@
+/*========================*\
+|  ID: tanishqbhargava2509 |
+|  LANG: C++               |
+\*========================*/
+//Tanishq Bhargava
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	int t;
+	cin >> t;
+	while(t--)
+	{
+		string s;	
+		cin >> s;
+		reverse(s.begin(), s.end());
+		cout << s << endl;
+	}
+}

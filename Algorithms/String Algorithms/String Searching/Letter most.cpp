f8867828ea22cd54d64ac563628e3926345ed84f
Tanishq Bhargava <66817044+TanishqBhargava@users.noreply.github.com>
2021-12-11 17:52:40 +0530

    Letter most.cpp

---
+++ Algorithms/String Algorithms/String Searching/Letter most.cpp
@@ -0,0 +1,18 @@
+/*========================*\
+|  ID: tanishqbhargava2509 |
+|  LANG: C++               |
+\*========================*/
+//Tanishq Bhargava
+#include<bits/stdc++.h> 
+using namespace std; 
+ 
+int main() 
+{ 
+	int n, hash[26] = {0}; 
+	cin >> n;
+   	string str; 
+	cin >> str;
+    for(char c : str) 
+      	hash[c - 'a']++;
+    cout << *max_element(hash, hash + 26);
+}

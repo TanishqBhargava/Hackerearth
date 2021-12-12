0d1f528c04f6b7b36934688767ef5d0d5312eb32
Tanishq Bhargava <66817044+TanishqBhargava@users.noreply.github.com>
2021-12-12 09:21:04 +0530

    String.cpp

---
+++ Algorithms/String Algorithms/String Searching/String.cpp
@@ -0,0 +1,19 @@
+/*========================*\
+|  ID: tanishqbhargava2509 |
+|  LANG: C++               |
+\*========================*/
+//Tanishq Bhargava
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+	int n, i, arr[26] = {0};;
+	cin >> n;
+	string s;
+	cin >> s;
+	for(i = 0; i < s.length(); i++)
+		arr[s[i] - 'a']++;
+	sort(arr, arr + 26);
+	cout << n - arr[25];
+}

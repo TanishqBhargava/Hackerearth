1543d65cadf055c361df8f6526472c7dcaa077df
Tanishq Bhargava <66817044+TanishqBhargava@users.noreply.github.com>
2021-12-30 17:27:46 +0530

    Chang and the Mathematical Brainbuster.cpp

---
+++ Algorithms/Dynamic Programming/Introduction to Dynamic Programming 1/Chang and the Mathematical Brainbuster.cpp
@@ -0,0 +1,28 @@
+/*========================*\
+|  ID: tanishqbhargava2509 |
+|  LANG: C++               |
+\*========================*/
+//Tanishq Bhargava
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+	int n;
+    cin >> n;
+    int v[n];
+    for(int i = 0; i < n; ++i)
+        cin >> v[i];
+    sort(v, v + n);
+    int i = 0, j = 0, res = 0;
+    while(i < n)
+    {
+        int b = 3 * v[i];
+        while(j < n && v[j] <= b)
+        	++j;
+        res = max(j - i, res);
+        ++i;
+    }
+    cout << n - res << endl;
+}

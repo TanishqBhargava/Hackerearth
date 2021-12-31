7cbe07116cbeac840e043ac1329a79b0ec9c2542
Tanishq Bhargava <66817044+TanishqBhargava@users.noreply.github.com>
2021-12-31 11:43:32 +0530

    Alice and candies.cpp

---
+++ Math/Number Throry/Basic Number Theory-1/Alice and candies.cpp
@@ -0,0 +1,23 @@
+/*========================*\
+|  ID: tanishqbhargava2509 |
+|  LANG: C++               |
+\*========================*/
+//Tanishq Bhargava
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long int n, cnt = 0;
+    cin >> n;
+    for(int i = 1; i * i <= n; i++)
+    {
+        if(n % i == 0)
+        {
+            int x = i, y = n / i;
+            if((x + y) % 2 == 0)
+                cnt++;
+        }
+    }
+    cout << cnt;
+}

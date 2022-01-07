f47611eace97971234a71d3344f0b3993a51c1c2
Tanishq Bhargava <66817044+TanishqBhargava@users.noreply.github.com>
2022-01-07 11:39:24 +0530

    In Love with Primes.cpp

---
+++ Math/Number Throry/Primality Tests/In Love with Primes.cpp
@@ -0,0 +1,22 @@
+/*========================*\
+|  ID: tanishqbhargava2509 |
+|  LANG: C++               |
+\*========================*/
+//Tanishq Bhargava
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    long long int t, n;
+    cin >> t;
+    while(t--)
+    {
+        cin >> n;
+        if(n == 2)
+            cout << "Arjit" << endl;
+        else
+            cout << "Deepa" << endl;
+    }
+}

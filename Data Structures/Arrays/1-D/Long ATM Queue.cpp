47132c70af30c65ac38a1ea0d1c663003a24d235
Tanishq Bhargava <66817044+TanishqBhargava@users.noreply.github.com>
2022-01-25 10:08:15 +0530

    Long ATM Queue.cpp

---
+++ Data Structures/Arrays/1-D/Long ATM Queue.cpp
@@ -0,0 +1,23 @@
+/*========================*\
+|  ID: tanishqbhargava2509 |
+|  LANG: C++               |
+\*========================*/
+//Tanishq Bhargava
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int n, x, p = 0, res = 1; 
+    cin >> n;
+    while(n--)
+    { 
+        cin >> x; 
+        if(x < p) 
+            res++; 
+        p = x; 
+    }
+    cout << res;
+    return 0;
+}

6fd61c1b32fbe01e7155da26dc7938754d9b44b0
Tanishq Bhargava <66817044+TanishqBhargava@users.noreply.github.com>
2022-01-31 05:58:39 -0800

    Cells in a matrix.cpp

---
+++ Data Structures/Hash Tables/Basics of Hash Tables/Cells in a matrix.cpp
@@ -0,0 +1,23 @@
+/*========================*\
+|  ID: tanishqbhargava2509 |
+|  LANG: C++               |
+\*========================*/
+//Tanishq Bhargava
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, m;
+	cin >> n >> m;
+	set<int> row, col;
+	for(int i = 0; i < m; i++)
+	{
+		int l, r;
+		cin >> l >> r;
+		row.insert(l);
+		col.insert(r);
+		cout << (n - row.size()) * 1LL * (n - col.size()) << " ";
+	}
+	return 0;
+}

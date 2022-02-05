803053a27c261985fa044ef3e059286b6f20ecc8
Tanishq Bhargava <66817044+TanishqBhargava@users.noreply.github.com>
2022-02-05 09:16:43 +0530

    Xsquare And Double Strings.cpp

---
+++ Data Structures/Hash Tables/Basics of Hash Tables/Xsquare And Double Strings.cpp
@@ -0,0 +1,30 @@
+/*========================*\
+|  ID: tanishqbhargava2509 |
+|  LANG: C++               |
+\*========================*/
+//Tanishq Bhargava
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+	long long int  t;
+    cin >> t;
+    while(t--)
+	{
+        string s;
+        cin >> s;
+        int hsh[26] = {0}, ct = 0;
+        for(int i = 0; i < s.size(); i++)
+            hsh[s[i] - 'a']++;
+        for(int i = 0; i < 26; i++)
+		{
+            if(hsh[i] >= 2)
+                ct++;
+        }
+        if(ct > 0)
+            cout << "Yes" << endl;
+        else
+            cout << "No" << endl;
+    }
+}

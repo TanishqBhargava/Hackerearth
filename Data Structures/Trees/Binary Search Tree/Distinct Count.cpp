7a9176f2244c46bcb317d4d1f1825d4cacdcdefd
Tanishq Bhargava <66817044+TanishqBhargava@users.noreply.github.com>
2022-02-09 10:06:47 +0530

    Distinct Count.cpp

---
+++ Data Structures/Trees/Binary Search Tree/Distinct Count.cpp
@@ -0,0 +1,30 @@
+/*========================*\
+|  ID: tanishqbhargava2509 |
+|  LANG: C++               |
+\*========================*/
+//Tanishq Bhargava
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int test;
+    cin >> test;
+    while(test--)
+    {
+        int n, x;
+        cin >> n >> x;
+        vector<int>v(n);
+        for(int i = 0; i < n; i++)
+            cin >> v[i];
+        sort(v.begin(), v.end());
+        int l = unique(v.begin(), v.end()) - v.begin();
+        if(l == x)
+            cout << "Good" << endl;
+        else if(l < x)
+            cout << "Bad" << endl;
+        else
+            cout << "Average" << endl;
+    }
+}

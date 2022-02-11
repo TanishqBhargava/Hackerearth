a82368ad2df36e79ad9491892febd34c90daa10f
Tanishq Bhargava <66817044+TanishqBhargava@users.noreply.github.com>
2022-02-11 18:26:34 +0530

    Changes in a string.cpp

---
+++ Algorithms/Greedy Algorithms/Basics of Greedy Algorithms/Changes in a string.cpp
@@ -0,0 +1,30 @@
+/*========================*\
+|  ID: tanishqbhargava2509 |
+|  LANG: C++               |
+\*========================*/
+//Tanishq Bhargava
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n, suf[1000014];
+        string s;
+        cin >> n >> s;
+        suf[n] = 0;
+        for(int i = n - 1; i >= 0; i--)
+            suf[i] = (s[i] == 'A') + suf[i + 1];
+        int pre = 0, ans = n;
+        for(int i = 0; i <= n; i++)
+        {
+            ans = min(ans, pre + suf[i]);
+            pre += s[i] == 'B';
+        }
+        cout << ans << endl;
+    }
+}

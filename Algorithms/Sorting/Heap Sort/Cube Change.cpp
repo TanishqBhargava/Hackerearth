af806030464f8e1e35cfb2c4bc1d6a1f74934ecf
Tanishq Bhargava <66817044+TanishqBhargava@users.noreply.github.com>
2022-02-12 11:30:47 +0530

    Cube Change.cpp

---
+++ Algorithms/Sorting/Heap Sort/Cube Change.cpp
@@ -0,0 +1,24 @@
+/*========================*\
+|  ID: tanishqbhargava2509 |
+|  LANG: C++               |
+\*========================*/
+//Tanishq Bhargava
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    unsigned long long t, n, cube, s = 0;
+    cin >> t;
+    while(t--)
+    {
+        cin >> n;
+        cube = n * n * n;
+        if(n > 2)
+        {
+            n -= 2;
+            s = n * n * n;
+        }
+        cout << cube - s << endl;
+    }
+}

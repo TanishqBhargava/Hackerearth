50f83f9f1d9d114d76d53ebe350fdd8287fb36de
Tanishq Bhargava <66817044+TanishqBhargava@users.noreply.github.com>
2022-02-26 11:01:26 +0530

    Xsquare And Array Operations.cpp

---
+++ Data Structures/Advanced Data Structures/Segment Trees/Xsquare And Array Operations.cpp
@@ -0,0 +1,26 @@
+/*========================*\
+|  ID: tanishqbhargava2509 |
+|  LANG: C++               |
+\*========================*/
+//Tanishq Bhargava
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int test, n, x, y;
+    cin >> test;
+    while(test--)
+    {
+        long long ans = 0;
+        cin >> n >> y;
+        while(--n)
+        {
+            cin >> x;
+            ans += max(x, y);
+            y = x;
+        }
+        cout << ans << endl;
+    }
+}

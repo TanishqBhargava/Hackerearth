71c80901af2f95b2178401c446158b0450c1fcb4
Tanishq Bhargava <66817044+TanishqBhargava@users.noreply.github.com>
2022-03-08 12:04:48 +0530

    Little Deepu and Array.cpp

---
+++ Data Structures/Advanced Data Structures/Segment Trees/Little Deepu and Array.cpp
@@ -0,0 +1,30 @@
+/*========================*\
+|  ID: tanishqbhargava2509 |
+|  LANG: C++               |
+\*========================*/
+//Tanishq Bhargava
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, m;
+    cin >> n;
+    long long int a[n];
+    for(int i = 1; i <= n; i++)
+        cin >> a[i];
+    cin >> m;
+    while(m--)
+    {
+        int x;
+        cin >> x;
+        int p = x; 
+        for(int i = 1; i <= n; i++)
+        {
+            if(a[i] > p)
+                a[i] -= 1;
+        }
+    }
+    for(int i = 1; i <= n; i++)
+        cout << a[i] << " ";
+}

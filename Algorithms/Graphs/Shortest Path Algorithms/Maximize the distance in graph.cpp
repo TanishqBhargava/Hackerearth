a8967270ba2e97665974b8f5c599d47f72883ccc
Tanishq Bhargava <66817044+TanishqBhargava@users.noreply.github.com>
2022-04-03 01:42:13 +0530

    Maximize the distance in graph.cpp

---
+++ Algorithms/Graphs/Shortest Path Algorithms/Maximize the distance in graph.cpp
@@ -0,0 +1,19 @@
+/*========================*\
+|  ID: tanishqbhargava2509 |
+|  LANG: C++               |
+\*========================*/
+//Tanishq Bhargava
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, k, x;
+    cin >> n >> k;
+    for(int i = 0; i < n; i++)
+    {
+        for(int j = 0; j < n; j++)
+            cin >> x;
+    }
+    cout << 0;
+}

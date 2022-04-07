f221ac76cc96cf77541b36af7098355243c72624
Tanishq Bhargava <66817044+TanishqBhargava@users.noreply.github.com>
2022-04-07 10:37:06 +0530

    ASCII Value.cpp

---
+++ Algorithms/String Algorithms/Z Algorithm/ASCII Value.cpp
@@ -0,0 +1,14 @@
+/*========================*\
+|  ID: tanishqbhargava2509 |
+|  LANG: C++               |
+\*========================*/
+//Tanishq Bhargava
+#include<bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    string C;
+    cin >> C;
+    cout << C[0] - 'a' + 97 << endl;
+}

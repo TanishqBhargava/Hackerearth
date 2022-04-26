d3ce3b4369431098424bd5f3ffde9f07d00959f4
Tanishq Bhargava <66817044+TanishqBhargava@users.noreply.github.com>
2022-04-26 15:03:05 +0530

    Array Splitting into two parts <P2SME>.cpp

---
+++ Algorithms/Dynamic Programming/2 Dimensional/Array Splitting into two parts <P2SME>.cpp
@@ -0,0 +1,21 @@
+/*========================*\
+|  ID: tanishqbhargava2509 |
+|  LANG: C++               |
+\*========================*/
+//Tanishq Bhargava
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    long long n, s = 0, x;
+    cin >> n;
+    for(int i = 0; i < n; i++)
+    {
+        cin >> x;
+        s += x;
+    }
+    long long ans = (s * s) / 4;
+    cout << ans;
+}

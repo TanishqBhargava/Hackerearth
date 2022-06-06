fdc34fa57496e462781fe9849099f2244d0c5041
Tanishq Bhargava <66817044+TanishqBhargava@users.noreply.github.com>
2022-06-06 08:19:24 +0530

    Binary Sequence.py

---
+++ Getting Started/Numbers/Binary Sequence.py
@@ -0,0 +1,14 @@
+ #========================#
+#  ID: tanishqbhargava2509 #
+#  LANG: Python 3          #
+ #========================#
+      #Tanishq Bhargava
+from sys import stdin
+ 
+t = int(stdin.readline())
+for _ in range(t):
+    x, y, a, b = map(int, stdin.readline().strip().split())
+    if x * y == a + b:
+        print('Yes')
+    else:
+        print('No')

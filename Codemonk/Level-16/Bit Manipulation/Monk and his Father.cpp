f7bb157f1bf88b77f113a40e242930c14bca9bf3
Tanishq Bhargava <66817044+TanishqBhargava@users.noreply.github.com>
2022-08-18 08:21:25 +0530

    Monk and his Father.cpp

---
+++ Codemonk/Level-16/Bit Manipulation/Monk and his Father.cpp
@@ -0,0 +1,20 @@
+/*========================*\
+|  ID: tanishqbhargava2509 |
+|  LANG: C++               |
+\*========================*/
+//Tanishq Bhargava
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int T;
+    cin >> T;
+    while(T--)
+	{
+        long long int P;
+        cin >> P;
+        cout << __builtin_popcountll(P) << "\n";
+    }
+}

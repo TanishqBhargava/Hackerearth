7a27994ed8927aed6cdf2926ccd5b961a8a3a9d0
Tanishq Bhargava <66817044+TanishqBhargava@users.noreply.github.com>
2022-11-10 08:05:30 +0530

    Swaps.cpp

---
+++ Basic Programming/Basics of Bit Manipulation/Swaps.cpp
@@ -0,0 +1,26 @@
+/*========================*\
+|  ID: tanishqbhargava2509 |
+|  LANG: C++               |
+\*========================*/
+//Tanishq Bhargava
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+	ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+    int t, n;
+    cin >> t;
+    while(t--)
+    {
+        cin >> n;
+		vector<int> a(n);
+		for(int i = 0; i < n; ++i)
+			cin >> a[i];
+		sort(a.begin(), a.end());
+		for(int i = 0; i < n; ++i)
+			cout << a[i] << ' ';
+        cout << endl;
+    }
+    return 0;
+}

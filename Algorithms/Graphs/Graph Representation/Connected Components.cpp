82265c11513369ecf88e9ce61fc58d1844d6c0a6
Tanishq Bhargava <66817044+TanishqBhargava@users.noreply.github.com>
2022-11-18 11:47:16 +0530

    Connected Components.cpp

---
+++ Algorithms/Graphs/Graph Representation/Connected Components.cpp
@@ -0,0 +1,30 @@
+/*========================*\
+|  ID: tanishqbhargava2509 |
+|  LANG: C++               |
+\*========================*/
+//Tanishq Bhargava
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios::sync_with_stdio(false); cin.tie(nullptr); cout.tie(nullptr);
+    vector<int> A(252525, 1);
+    A[0] = 0;
+    for(int i = 2; i * i < 252525; i++)
+    {
+        for(int j = i * i; j < 252525; j += i * i)
+            A[j] = 0;
+    }
+    for(int i = 2; i < 252525; i++)
+        A[i] += A[i - 1];
+    int t, n;
+    cin >> t;
+    while(t > 0)
+    {
+        t--;
+        cin >> n;
+        cout << A[n] << "\n";
+    }
+    return 0;
+}

ab8e720257bbfa062e14f48b1a15fa73fcdd300f
Tanishq Bhargava <66817044+TanishqBhargava@users.noreply.github.com>
2022-11-28 23:21:25 +0530

    MAX AND.cpp

---
+++ Basic Programming/Basics of Bit Manipulation/MAX AND.cpp
@@ -0,0 +1,31 @@
+/*========================*\
+|  ID: tanishqbhargava2509 |
+|  LANG: C++               |
+\*========================*/
+//Tanishq Bhargava
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+	ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    int T, n;
+    cin >> T;
+    for(int tc = 1; tc <= T; tc++)
+    {
+        cin >> n;
+        vector<int> a(n), b(n);
+        for(int i = 0; i < n; i++)
+            cin >> a[i];
+        for(int i = 0; i < n; i++) 
+            cin >> b[i];
+        int ans = (1LL << 32) - 1;
+        for(int i = 0; i < n; i++) 
+        {
+            ans = ans & a[i];
+            ans = ans & b[i];
+        }
+        cout << ans << "\n";
+    }
+    return 0;  
+}

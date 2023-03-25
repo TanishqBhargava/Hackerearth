05966b820eb94a87803d8fc6166970cd259ac2fd
Tanishq Bhargava <66817044+TanishqBhargava@users.noreply.github.com>
2023-03-25 08:43:16 +0530

    Telephone connections.cpp

---
+++ Algorithms/Dynamic Programming/Introduction to Dynamic Programming 1/Telephone connections.cpp
@@ -0,0 +1,21 @@
+/*========================*\
+|  ID: tanishqbhargava2509 |
+|  LANG: C++               |
+\*========================*/
+//Tanishq Bhargava
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main()
+{
+    long long n, m, kmin, kmax, x;
+    cin >> n >> m;
+    x = n / m + 1;
+    kmin = ((x * (x - 1)) / 2) * (n % m);
+    x -= 1;
+    kmin += ((x * (x - 1)) / 2) * (m - (n % m));
+    x = n - m + 1;
+    kmax = (x * (x - 1)) / 2;
+    cout << kmin << " " << kmax;
+    return 0;
+}

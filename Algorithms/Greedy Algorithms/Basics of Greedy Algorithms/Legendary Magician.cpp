9474eee5ce286b460367684a06f36d9ef60bc57a
Tanishq Bhargava <66817044+TanishqBhargava@users.noreply.github.com>
2023-04-14 07:37:21 +0530

    Legendary Magician.cpp

---
+++ Algorithms/Greedy Algorithms/Basics of Greedy Algorithms/Legendary Magician.cpp
@@ -0,0 +1,36 @@
+/*========================*\
+|  ID: tanishqbhargava2509 |
+|  LANG: C++               |
+\*========================*/
+//Tanishq Bhargava
+#include<bits/stdc++.h>
+using namespace std;
+    
+int main()
+{
+    int t; 
+    cin >> t;
+    while(t--)
+    {   
+        int n, ans = 0, prev = 0; 
+        cin >> n;
+        vector<pair<int, int>> a(n);
+        for(int i = 0; i < n; i++)
+            cin >> a[i].first;
+        for(int i = 0; i < n; i++)
+            cin >> a[i].second;
+        sort(a.begin(), a.end());
+        for(int i = 0; i < n; i++)
+        {
+            if(a[i].first > prev)
+            {
+                prev = a[i].second;
+                ans++;
+            }
+            else
+                prev = min(prev, a[i].second);
+        }
+        cout << ans << endl;
+    }
+    return 0;
+}

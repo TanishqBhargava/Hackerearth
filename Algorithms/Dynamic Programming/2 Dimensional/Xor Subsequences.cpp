1879bb64d57758b13e10ab3dffff965f61ed58da
Tanishq Bhargava <66817044+TanishqBhargava@users.noreply.github.com>
2023-05-28 09:09:24 +0530

    Xor Subsequences.cpp

---
+++ Algorithms/Dynamic Programming/2 Dimensional/Xor Subsequences.cpp
@@ -0,0 +1,34 @@
+/*========================*\
+|  ID: tanishqbhargava2509 |
+|  LANG: C++               |
+\*========================*/
+//Tanishq Bhargava
+#include<bits/stdc++.h>
+using namespace std;
+
+int main() 
+{
+    int n;
+    cin >> n;
+    vector<int> v(n);
+    for(int i = 0; i < n; i++) 
+        cin >> v[i];
+    vector<bool> freq(1024, false);
+    for(auto ele : v) 
+	{
+        for(int i = 0; i < 1024; i++) 
+		{
+            if(freq[i]) 
+                freq[i ^ ele] = true;
+        }
+        freq[ele] = true;
+        int c = 0;
+        for(int i = 0; i < 1024; i++) 
+		{
+            if(freq[i]) 
+                c++;
+        }
+        cout << c << ' ';
+    }
+	cout << endl;
+}

fa123b34c4719de962a795cd9c3ffa645b28aa33
Tanishq Bhargava <66817044+TanishqBhargava@users.noreply.github.com>
2023-12-28 09:09:59 +0530

    Find pairs-orderedpairs.cpp

---
+++ Basic Programming/Basics of Implementation/Find pairs-orderedpairs.cpp
@@ -0,0 +1,25 @@
+/*========================*\
+|  ID: tanishqbhargava2509 |
+|  LANG: C++               |
+\*========================*/
+//Tanishq Bhargava
+#include <bits/stdc++.h>
+using namespace std;
+ 
+int main() 
+{
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
+    int n, c = 0;
+    cin >> n;
+    vector<int> a(n);
+    map<int, int> m;
+    for(int i = 0; i < n; i++) 
+    {
+        cin >> a[i];
+        m[a[i] - i] += 1;
+    }
+    for(auto i : m) 
+        c += (i.second * (i.second - 1));
+    cout << c << endl;
+    return 0;  
+}
